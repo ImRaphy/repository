9ec74c60683dd5d6c22c35119b3742af9ff0070d
ImRaphy <mail@raphael-s.com>
2013-04-03 16:40:26 +0900

    Hello!

---
+++ pathes.php
@@ -0,0 +1,11 @@
+<?php
+echo '<span style="color:red;">__FILE__: </span>' . __FILE__ . '<br>';
+echo '<span style="color:red;">__DIR__: </span>' . __DIR__ . '<br>';
+echo '<span style="color:red;">$_SERVER[\'SERVER_NAME\']: </span>' . $_SERVER['SERVER_NAME'] . '<br>';
+echo '<span style="color:red;">$_SERVER[\'QUERY_STRING\']: </span>' . $_SERVER['QUERY_STRING'] . '<br>';
+echo '<span style="color:red;">$_SERVER[\'REQUEST_URI\']: </span>' . $_SERVER['REQUEST_URI'] . '<br>';
+echo '<span style="color:red;">$_SERVER[\'PHP_SELF\']: </span>' . $_SERVER['PHP_SELF'] . '<br>';
+echo '<span style="color:red;">$_SERVER[\'DOCUMENT_ROOT\']: </span>' . $_SERVER['DOCUMENT_ROOT'] . '<br>';
+echo '<span style="color:red;">$_SERVER[\'SCRIPT_FILENAME\']: </span>' . $_SERVER['SCRIPT_FILENAME'] . '<br>';
+echo '<span style="color:red;">$_SERVER[\'SCRIPT_NAME\']: </span>' . $_SERVER['SCRIPT_NAME'] . '<br>';
+?>
